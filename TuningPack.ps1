d0a81d6c0c69d92704f77d0f00a323d22a2191fa
MinersWin <minerswin@minerswin.de>
2020-02-15 17:03:01 +0100

    Update!

--- TuningPack.ps1
+++ TuningPack.ps1
@@ -6,47 +6,63 @@ while (!(test-connection 8.8.8.8 -Count 1 -Quiet)) {
     $Verbindungbesteht = $true
     break
 }
-
 if ($Verbindungbesteht){
     $Internet = $false
 } else {
     $Internet = $true
 }
-
-
 if ($Internet){
-    "Internetverbindung: Online"
+    "$(Get-Date) Internetverbindung: Online"
 } else {
-    "Internetverbindung: Offline"
+    "$(Get-Date) Internetverbindung: Offline"
 }
 ##########################################################################################################################################################################
 
 $Gedownloaded = Test-Path .\TuningPack\
 if ($Gedownloaded){
-    Write-Output "Das Tuning Pack wurde bereits heruntergeladen"
+    Write-Output "$(Get-Date) Das Tuning Pack wurde bereits heruntergeladen"
 } else {
     if ($Internet){
-        Write-Output "Das Tuning Pack muss heruntergeladen werden"
+        Write-Output "$(Get-Date) Das Tuning Pack muss heruntergeladen werden"
     } else {
-        Write-Output "Das Tuning Pack ist nicht Installiert, jedoch besteht keine Internetverbindung. Das Programm wird beendet."
+        Write-Output "$(Get-Date) Das Tuning Pack ist nicht Installiert, jedoch besteht keine Internetverbindung. Das Programm wird beendet."
     }
 }
 
 if ($Internet){
-    $NeusteVersion = $Downloadlink = curl http://root3.minerswin.de/TGF/TuningPack/LatestVersion.html -UseBasicParsing
-    Write-Output "Neuste Version: $($NeusteVersion.Content)"
+    $NeusteVersion = Invoke-WebRequest http://root3.minerswin.de/TGF/TuningPack/LatestVersion.html -UseBasicParsing
+    Write-Output "$(Get-Date) Neuste Version: $($NeusteVersion)"
     if ($Gedownloaded){
-        Set-Location .\TuningPack\
-        Start-Process powershell .\GUI.ps1
+        $VersionInstalliert = Get-Content .\TuningPack\Version.txt
+        if ($NeusteVersion.Content -gt $VersionInstalliert){
+            Write-Output "$(Get-Date) Die aktuelle Version wird nicht mehr Unterstüzt. Die neuste wird heruntergeladen."
+            Write-Output "$(Get-Date) Tuning Pack wurde beendet"
+            del .\TuningPack -Recurse -Force
+            Write-Output "$(Get-Date) Tuning Pack wurde gelöscht"
+            $Downloadlink = curl http://root3.minerswin.de/TGF/TuningPack/DownloadLink.html -UseBasicParsing
+            Write-Output "$(Get-Date) Der Download wird gestartet"
+            wget $Downloadlink.Content -OutFile 'TuningPack.zip' -UseBasicParsing
+            Write-Output "$(Get-Date) Der Download wurde abgeschlossen"
+            Expand-Archive .\TuningPack.zip -DestinationPath .\TuningPack\
+            rm TuningPack.zip
+            Set-Location .\TuningPack\
+            Write-Output "$(Get-Date) Das Tuning Pack wird gestartet"
+            $NeusteVersion.Content | Out-File -FilePath .\Version.txt
+            Start-Process powershell .\GUI.ps1
+        } else {
+            Set-Location .\TuningPack\
+            Start-Process powershell .\GUI.ps1
+        }
     } else {
         $Downloadlink = curl http://root3.minerswin.de/TGF/TuningPack/DownloadLink.html -UseBasicParsing
-        Write-Output "Der Download wird gestartet"
+        Write-Output "$(Get-Date) Der Download wird gestartet"
         wget $Downloadlink.Content -OutFile 'TuningPack.zip' -UseBasicParsing
-        Write-Output "Der Download wurde abgeschlossen"
+        Write-Output "$(Get-Date) Der Download wurde abgeschlossen"
         Expand-Archive .\TuningPack.zip -DestinationPath .\TuningPack\
         rm TuningPack.zip
         Set-Location .\TuningPack\
-        Write-Output "Das Tuning Pack wird gestartet"
+        Write-Output "$(Get-Date) Das Tuning Pack wird gestartet"
+        $NeusteVersion.Content | Out-File -FilePath .\Version.txt
         Start-Process powershell .\GUI.ps1
     }
 } else {
